3fbd9889a4fc38079621cf30bbb796dd3a8c175d
Kacper Kotlewski <kotlewski.kacper@gmail.com>
2023-12-25 19:15:20 +0100

    new parser test

--- project/tests/client/schema/deserialize/test_parser.py
+++ project/tests/client/schema/deserialize/test_parser.py
@@ -1,5 +1,5 @@
 import pytest
-from client.schema.deserialize import parser
+from client.schema.deserialize import parser, exceptions as exc
 
 def test_get_key_and_value():
     lines = {
@@ -7,11 +7,15 @@ def test_get_key_and_value():
         "key_int : 123": ("key_int", "123"),
         "key_list1 : x,y,z": ("key_list1", "x,y,z"),
         "key_list2 : [x,y,z]": ("key_list2", "[x,y,z]"),
+        "empty": ("empty", None),
+        "error: : error": exc.EnvSchemaParsingError,
     }
     
     for line, expected in lines.items():
-        (key,value) = parser.get_key_and_value(line)
-        assert key == expected[0]        
-        assert value == expected[1]        
-    
+        try:
+            (key,value) = parser.get_key_and_value(line)
+            assert key == expected[0]        
+            assert value == expected[1]
+        except Exception as e:
+            assert isinstance(e, expected)
     
