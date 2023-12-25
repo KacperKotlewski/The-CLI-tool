0bf8cbb225ae7d0a966fa83bdd65b8e8a7089c7f
Kacper Kotlewski <kotlewski.kacper@gmail.com>
2023-12-25 19:18:12 +0100

    parser test versions

--- project/tests/client/schema/deserialize/test_parser.py
+++ project/tests/client/schema/deserialize/test_parser.py
@@ -54,3 +54,47 @@ def test_parse_cli_config_first_line_invalid():
         assert True
     else:
         assert False, "EnvSchemaNotFound exception not raised"
+        
+def test_parse_cli_config_first_line_valid():
+    """
+    test_parse_cli_config_1 test checking of the first line of the schema file containing "dotEnv schema"
+    """
+    data = parser.ParseData(
+        line="dotEnv schema",
+        line_count=0,
+        schema_model=models.Schema(),
+        flag=None
+    )
+    data = parser.parse_cli_config(data)
+    
+    assert data.line_count == 1
+    assert data.flag == None
+    assert isinstance(data.schema_model, models.Schema)
+    assert data.schema_model.schematizerVersion == None
+    
+def test_parse_cli_config_second_line_valid():
+    """
+    test_parse_cli_config_2 test checking of the second line of the schema file containing "CliVersion"
+    """
+    versions = {
+        "0.1": Version.v0_1,
+        "0.1.1": Version.v0_1_1,
+        "0.1.x": Version.v0_1_1,
+        "0.x": Version.v0_1_1,
+        "1.0": exc.EnvSchemaParsingError,
+    }
+    for version, expected in versions.items():
+        data = parser.ParseData(
+            line=f"CliVersion: {version}",
+            line_count=1,
+            schema_model=models.Schema(),
+            flag=None
+        )
+        try:
+            data = parser.parse_cli_config(data)
+            assert data.line_count == 2
+            assert data.flag == None
+            assert isinstance(data.schema_model, models.Schema)
+            assert data.schema_model.schematizerVersion == expected
+        except Exception as e:
+            assert expected == e.__class__    
