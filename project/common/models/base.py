59e2701cd48867183c537f198a87e82324675b48
Kacper Kotlewski <kotlewski.kacper@gmail.com>
2023-12-25 20:31:17 +0100

    new base model for validation

---
+++ project/common/models/base.py
@@ -0,0 +1,45 @@
+from pydantic import ValidationError, BaseModel as PydanticBaseModel
+
+
+class BaseModel(PydanticBaseModel):
+    def Copy(self):
+        """
+        Copy all values to a new model
+        """
+        data = self.model_dump()
+        new_model = self.__class__(**data)
+        return new_model
+
+    def CopyFiltered(self):
+        """
+        Copy filtered values to a new model
+        Filtered values:
+            - None
+        """
+        data = {k: v for k, v in self.model_dump().items() if v is not None}
+        new_model = self.__class__(**data)
+        return new_model
+
+    def isValid(self) -> bool:
+        """
+        Validate model
+        """
+        try:
+            copy = self.Copy()
+            del copy
+        except ValidationError:
+            return False
+        else:
+            return True
+        
+    def isValidFiltered(self) -> bool:
+        """
+        Validate model
+        """
+        try:
+            copy = self.CopyFiltered()
+            del copy
+        except ValidationError:
+            return False
+        else:
+            return True
---
+++ project/tests/common/models/test_base.py
@@ -0,0 +1,109 @@
+import typing
+from common.models.base import BaseModel, ValidationError
+
+class TestBaseModel(BaseModel):
+    value1: str = None
+    value2: bool = None
+    value3: typing.List[str] = list()
+    value4: typing.Dict[str, str] = dict()
+    value5: typing.Optional[str] = None
+    
+def test_base_model_copy():
+    """
+    test_base_model test BaseModel
+    """
+    model = TestBaseModel()
+    model.value1 = "value1"
+    model.value2 = True
+    model.value3 = ["value3"]
+    model.value4 = {"key": "value"}
+    
+    copy = model.Copy()
+    
+    assert copy.value1 == model.value1 == "value1"
+    assert copy.value2 == model.value2 == True
+    assert copy.value3 == model.value3 == ["value3"]
+    assert copy.value4 == model.value4 == {"key": "value"}
+    assert copy.value5 == model.value5 == None
+    
+def test_base_model_copy_invalid():
+    """
+    test_base_model test BaseModel
+    """
+    model = TestBaseModel()
+    
+    try:
+        model.Copy()
+    except Exception as e:
+        assert isinstance(e, ValidationError)
+    else:
+        assert False, "TypeError exception not raised"
+    
+def test_base_model_copy_filtered():
+    """
+    test_base_model_filtered test BaseModel
+    """
+    model = TestBaseModel()
+    model.value2 = True
+    
+    copy = model.CopyFiltered()
+    
+    assert copy.value1 == model.value1 == None
+    assert copy.value2 == model.value2 == True
+    assert copy.value3 == model.value3 == []
+    assert copy.value4 == model.value4 == {}
+    assert copy.value5 == model.value5 == None
+    
+def test_base_model_copy_filtered_invalid():
+    """
+    test_base_model_filtered test BaseModel
+    """
+    model = TestBaseModel()
+    
+    model.value1 = True
+        
+    try:
+        model.CopyFiltered()
+    except Exception as e:
+        assert isinstance(e, ValidationError)
+    else:
+        assert False, "ValidationError exception not raised"
+    
+def test_base_model_is_valid():
+    """
+    test_base_model_is_valid test BaseModel
+    """
+    model = TestBaseModel()
+    model.value1 = "value1"
+    model.value2 = True
+    model.value3 = ["value3"]
+    model.value4 = {"key": "value"}
+    
+    assert model.isValid() == True
+    
+def test_base_model_is_valid_invalid():
+    """
+    test_base_model_is_valid test BaseModel
+    """
+    model = TestBaseModel()
+        
+    assert model.isValid() == False
+    
+def test_base_model_is_valid_filtered():
+    """
+    test_base_model_is_valid_filtered test BaseModel
+    """
+    model = TestBaseModel()
+    model.value2 = True
+    
+    assert model.isValidFiltered() == True
+    
+def test_base_model_is_valid_filtered_invalid():
+    """
+    test_base_model_is_valid_filtered test BaseModel
+    """
+    model = TestBaseModel()
+    
+    model.value1 = True
+        
+    assert model.isValidFiltered() == False
