c3bd45c056a195ea9230d4070555a7e247d210bd
Kacper Kotlewski <kotlewski.kacper@gmail.com>
2023-12-25 19:16:02 +0100

    new version

--- project/client/schema/versions.py
+++ project/client/schema/versions.py
@@ -1,20 +1,66 @@
-import re
+import enum
+import typing
 
-VALID_SCHEMA_VERSIONS = [
-    "0.1", "0.1.1",
-]
+class InvalidVersion(Exception):
+    pass
 
-def checkVersion(version:str) -> bool:
-    if version in VALID_SCHEMA_VERSIONS:
-        return True
-    if 'x' in version.lower():
-        vsplit = version.split(".")
-        for validV in VALID_SCHEMA_VERSIONS:
-            validVsplit = validV.split(".")
-            if vsplit[0] == validVsplit[0]:
-                for i,v in enumerate(vsplit[1:]):
-                    if v.lower() == "x":
-                        return True
-                    elif v.lower() != validVsplit[i+1].lower():
-                        break
-    return False
+class Version(enum.Enum):
+    v0_1 = "0.1"
+    v0_1_1 = "0.1.1"
+
+    @staticmethod
+    def getAll() -> typing.List['Version']:
+        return [v for v in Version]
+
+    @staticmethod
+    def getAllStr() -> typing.List[str]:
+        return [v.value for v in Version]
+
+    @staticmethod
+    def getLatest() -> 'Version':
+        return Version.getAll()[-1]
+
+    @staticmethod
+    def getLatestMajor() -> str:
+        return Version.getAll()[-1].value.split(".")[0]
+
+    @staticmethod
+    def check(version: str) -> bool:
+        if version in Version.getAllStr():
+            return True
+        if 'x' in version.lower():
+            vsplit = version.split(".")
+            for validV in Version.getAllStr():
+                validVsplit = validV.split(".")
+                if vsplit[0] == validVsplit[0]:
+                    for i, v in enumerate(vsplit[1:]):
+                        if v.lower() == "x":
+                            return True
+                        elif i + 1 < len(validVsplit) and v.lower() != validVsplit[i + 1].lower():
+                            break
+        return False
+
+    @staticmethod
+    def match(version_str: str) -> 'Version':
+        for v in Version:
+            if v.value == version_str:
+                return v
+        raise InvalidVersion(f"Version string '{version_str}' does not match any Version enum")
+
+    @staticmethod
+    def fetch(version: str) -> 'Version':
+        if version in Version.getAllStr():
+            return Version.match(version)
+        if 'x' in version.lower():
+            vsplit = version.split(".")
+            reversed_versions = Version.getAllStr()
+            reversed_versions.reverse()
+            for validV in reversed_versions:
+                validVsplit = validV.split(".")
+                if vsplit[0] == validVsplit[0]:
+                    for i, v in enumerate(vsplit[1:]):
+                        if v.lower() == "x" or (i + 1 < len(validVsplit) and v.lower() == validVsplit[i + 1].lower()):
+                            return Version.match(validV)
+                        elif i + 1 >= len(validVsplit) or v.lower() != validVsplit[i + 1].lower():
+                            break
+        raise InvalidVersion(f"Invalid version: {version}")
--- project/tests/client/schema/test_version.py
+++ project/tests/client/schema/test_version.py
@@ -1,12 +1,45 @@
 import pytest
-from client.schema.versions import checkVersion, VALID_SCHEMA_VERSIONS
+from client.schema.versions import Version, InvalidVersion
+    
+def test_latest():
+    latest = Version.getLatest()
+    latestMajor = Version.getLatestMajor()
+    
+    assert latest == Version.v0_1_1
+    assert latestMajor == "0" 
 
-def test_checkVersion():
-    validVersions = VALID_SCHEMA_VERSIONS + ["0.x", "0.x.x", "0.1.x"]
+def test_check():
+    validVersions = Version.getAllStr() + ["0.x", "0.x.x", "0.1.x"]
     notValidVersions = ["x.x", "invalid", "9999.9999"]
     
     for version in validVersions:
-        assert checkVersion(version), f"Version '{version}' should be valid"
+        assert Version.check(version), f"Version '{version}' should be valid"
 
     for version in notValidVersions:
-        assert not checkVersion(version), f"Version '{version}' should be invalid"
+        assert not Version.check(version), f"Version '{version}' should be invalid"
+        
+def test_version_content():
+    versions = Version.getAll()
+    versionsStr = Version.getAllStr()
+    
+    assert len(versions) == len(versionsStr)
+    assert versions != versionsStr
+    assert len(versions) == len(Version)
+    
+def test_match():
+    version = Version.v0_1_1.value
+    assert Version.match(version) == Version.v0_1_1
+    
+def test_match_invalid():
+    version = "invalid"
+    with pytest.raises(InvalidVersion):
+        Version.match(version)
+
+def test_fetch():
+    versions = Version.getAllStr() + ["0.x", "0.x.x", "0.1.x"]
+    
+    for version in versions:
+        if "x" in version:
+            assert Version.fetch(version) == Version.getLatest()
+        else:
+            assert Version.fetch(version).value == version
