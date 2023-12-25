8d6873f694f4e7078393ccd1516f9476cbee5e1f
Kacper Kotlewski <kotlewski.kacper@gmail.com>
2023-12-25 19:17:12 +0100

    parser data type

---
+++ project/client/schema/deserialize/models.py
@@ -0,0 +1,9 @@
+import pydantic
+import typing
+from .. import models
+
+class ParseData(pydantic.BaseModel):
+    line: str
+    line_count: int
+    schema_model: typing.Union[models.Schema, models.SchemaInfo, models.SchemaElement]
+    flag: typing.Optional[bool]
