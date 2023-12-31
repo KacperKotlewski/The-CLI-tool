207a8127193eb04b4acd8e6684ee446a4dcf571b
Kacper Kotlewski <kotlewski.kacper@gmail.com>
2023-12-31 11:55:26 +0100

    changed base model

--- project/client/schema/models/env.py
+++ project/client/schema/models/env.py
@@ -1,10 +1,10 @@
 import typing
-import pydantic
+from common.models.base import BaseModel
 from .info import SchemaInfo
 from .elements import SchemaElement
 from ..versions import Version
 
-class EnvSchema(pydantic.BaseModel):
+class EnvSchema(BaseModel):
     schematizerVersion: Version = None
     schemaInfo: SchemaInfo = None
     elements: typing.List[SchemaElement] = list()
