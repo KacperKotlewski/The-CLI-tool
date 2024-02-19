74f8308aadc327a6074bd126190687a29ef69aac
Kacper Kotlewski <kotlewski.kacper@gmail.com>
2024-02-19 19:14:51 +0100

    root update

--- project/common/CLI/module/root_module.py
+++ project/common/CLI/module/root_module.py
@@ -1,15 +1,22 @@
+from pydantic import Field
 from .module import Module
 
 import typing
 
 from common.CLI.option import OptionFactory, Option, OptionBuilder
 from common.CLI.action import ActionFactory, ActionBuilder, ActionHandler
+# from common.CLI.interface import UserInterface
 
+from common.SOLID import Singleton
 from common.debug import DEBUG
 
 import sys
 
-class RootModule(Module):
+class RootModule(Singleton, Module):
+    
+    def __init__(self, **data) -> None:
+        super().__init__(**data)
+        self.root_module = self        
     
     def print_help_usage_action(self, *args) -> None:
         print(f'Use "{sys.argv[0]} -h" or "{sys.argv[0]} --help" for help and information.')
@@ -19,6 +26,7 @@ class RootModule(Module):
     
     def _extra_options_and_actions(self) -> None:
         self._append_debug_option()
+        self._append_interface_option()
         
     def _append_debug_option(self) -> None:
         builder = OptionBuilder().set_name('debug')
@@ -42,3 +50,21 @@ class RootModule(Module):
             function=lambda *args: DEBUG.set_keyword(debug_option.value)
         ))
         
+    def _append_interface_option(self) -> None:
+        builder = OptionBuilder().set_name('interface')
+        builder.add_keys('-U', '--user-interface')
+        builder.set_description('Set the user interface to use. Default is cli.')
+        builder.set_option('type')
+        builder.set_value('cli')
+        
+        interface_option = builder.build_option()
+        self.option_handler += interface_option
+        
+        # self.action_handler.insert_action(0, ActionFactory.from_option(
+        #     option=interface_option, 
+        #     condition=lambda *args: isinstance(interface_option.value, str),
+        #     function=lambda *args: self.interface_handler.set_active(interface_option.value)
+        # ))
+    
+    # def get_ui(self) -> 'UserInterface':
+    #     return self.interface_handler.active
